0f609a6481f7408e548a02d87a80a00e3650551f
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-03-22 17:44:34 +0200

    JSON Web Token Strategy to step 9

--- week2/app.js
+++ week2/app.js
@@ -6,10 +6,15 @@ const app = express();
 const port = 3000;
 const cats = require("./routes/catRoute");
 const users = require("./routes/userRoute");
+const auth = require("./routes/authRoute");
+const passport = require("./utils/pass");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", auth);
+app.use("/user", passport.authenticate("jwt", { session: false }), users);
 
 app.use("/cat", cats);
 app.use("/user", users);
--- week2/models/userModel.js
+++ week2/models/userModel.js
@@ -1,19 +1,32 @@
-'use strict';
+"use strict";
 const users = [
   {
-    id: '1',
-    name: 'John Doe',
-    email: 'john@metropolia.fi',
-    password: '1234',
+    id: "1",
+    name: "John Doe",
+    email: "john@metropolia.fi",
+    password: "1234"
   },
   {
-    id: '2',
-    name: 'Jane Doez',
-    email: 'jane@metropolia.fi',
-    password: 'qwer',
-  },
+    id: "2",
+    name: "Jane Doez",
+    email: "jane@metropolia.fi",
+    password: "qwer"
+  }
 ];
 
+const getUserLogin = async params => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+      "SELECT * FROM wop_user WHERE email = ?;",
+      params
+    );
+    return rows;
+  } catch (e) {
+    console.log("error", e.message);
+  }
+};
+
 module.exports = {
-  users,
+  users
 };
