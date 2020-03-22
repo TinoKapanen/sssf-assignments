5a2eab48fa70d787f366e1452c50a4c8233480a2
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-03-22 10:36:29 +0200

    fixes to app.js and catRoute.js

--- week2/app.js
+++ week2/app.js
@@ -2,27 +2,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const catModel = require("./models/catModel.js");
-const cats = catModel.cats;
+const cats = require("./routes/catRoute");
+const users = require("./routes/userRoute");
 
-app.get("/cat", (req, res) => {
-  res.send("From this endpoint you can get cats.");
-});
-
-app.get("/cat/:id", function(req, res) {
-  res.send(cats);
-});
-
-app.post("/cat", function(req, res) {
-  res.send("With this endpoint you can add cats.");
-});
-
-app.put("/cat", function(req, res) {
-  res.send("With this endpoint you can edit cats.");
-});
-
-app.delete("/cat", function(req, res) {
-  res.send("With this endpoint you can delete cats.");
-});
+app.use("/cat", cats);
+app.use("/user", users);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
