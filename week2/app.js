d7814e8af7c04dada8b3140270a4a50fb368671a
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-03-22 10:24:27 +0200

    routes and controllers changes for car and user

--- week2/app.js
+++ week2/app.js
@@ -2,9 +2,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const catModel = require("./models/catModel.js");
+const cats = catModel.cats;
 
 app.get("/cat", (req, res) => {
   res.send("From this endpoint you can get cats.");
+});
+
+app.get("/cat/:id", function(req, res) {
+  res.send(cats);
 });
 
 app.post("/cat", function(req, res) {
