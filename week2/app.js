2325a48d484307e4f1ccb2f6d6e955217fef4011
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-03-22 14:50:52 +0200

    week2 Routing and Middleware

--- week2/app.js
+++ week2/app.js
@@ -1,9 +1,15 @@
 "use strict";
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const cats = require("./routes/catRoute");
 const users = require("./routes/userRoute");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/cat", cats);
 app.use("/user", users);
--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -2,13 +2,15 @@
 // catRoute
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/" });
 const catController = require("../controllers/catController");
 
 router.get("/", catController.cat_list_get);
 
 router.get("/:id", catController.cat_get);
 
-router.post("/", catController.cat_post);
+router.post("/", upload.single("cat"), catController.cat_post);
 
 router.put("/", (req, res) => {
   res.send("With this endpoint you can edit cats");
