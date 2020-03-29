c3dd636d29a39aa45bd48e35ff1a310be0e15df6
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-03-29 21:10:05 +0300

    app.js changes

--- week2/app.js
+++ week2/app.js
@@ -6,12 +6,15 @@ const app = express();
 const port = 3000;
 const cats = require("./routes/catRoute");
 const users = require("./routes/userRoute");
+const auth = require("./routes/authRoute");
+const passport = require(".utils/pass");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/cat", cats);
-app.use("/user", users);
+app.use("/cat", passport.auhenticate('jwt', {session: false}), cats);
+app.use("/user", passport.auhenticate('jwt', {session: false}), users);
+app.use("/auth", auth);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
