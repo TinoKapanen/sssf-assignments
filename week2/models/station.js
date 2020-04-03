dd24aa097fb5a0542944ce5070165fe416a4b789
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-04-03 18:23:22 +0300

    chargemap updates

---
+++ week2/models/station.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const stationSchema = new Schema({
+  //TODO: schema
+});
+
+module.exports = mongoose.model('Station', stationSchema);
