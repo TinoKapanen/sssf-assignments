2ba61a21c18a6f6c6234ebc56fa64c66ea1d2ab8
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-04-09 15:28:28 +0300

    models updated

--- week2/models/station.js
+++ week2/models/station.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Connection = require('./connection');
 
 const Schema = mongoose.Schema;
 
--- week2/models/user.js
+++ week2/models/user.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  full_name: String,
+  username: {type: String, unique: true},
+  password: {type: String, required: true},
+  full_name: {type: String, required: true},
 });
 
 module.exports = mongoose.model('User', userSchema);
