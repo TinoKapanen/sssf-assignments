b2b07db44ed3e317f0d36aa7b3364da611ddb3c0
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-04-09 15:17:38 +0300

    Update authController.js

--- week2/controllers/authController.js
+++ week2/controllers/authController.js
@@ -3,27 +3,42 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
 const login = (req, res) => {
-  passport.authenticate('local', {session: false}, async (err, user, info) => {
-    try {
-      console.log('controller info', info);
+  return new Promise((resolve, reject) => {
+    passport.authenticate('local', {session: false},
+        async (err, user, info) => {
+          try {
+            console.log('controller info', info);
+            if (err || !user) {
+              reject(info.message);
+            }
+            req.login(user, {session: false}, async (err) => {
+              if (err) {
+                reject(err);
+              }
+              // generate a signed son web token with the contents of user object and return it in the response
+              const token = jwt.sign(user, 'asd123');
+              resolve({user, token});
+            });
+          }
+          catch (e) {
+            reject(e.message);
+          }
+        })(req, res);
+  });
+};
+
+const checkAuth = (req, res) => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate('jwt', (err, user) => {
       if (err || !user) {
-        throw new Error(info.message);
+        reject('Not authenticated or user expired');
       }
-      req.login(user, {session: false}, async (err) => {
-        if (err) {
-          throw new Error(err);
-        }
-        // generate a signed son web token with the contents of user object and return it in the response
-        const token = jwt.sign(user, 'asd123');
-        return res.json({user, token});
-      });
-    }
-    catch (e) {
-      res.status(500).json({message: e.message});
-    }
-  })(req, res);
+      resolve(user);
+    })(req, res);
+  });
 };
 
 module.exports = {
   login,
+  checkAuth,
 };
