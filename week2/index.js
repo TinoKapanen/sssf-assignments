9811b035181e33bae13e7046b0b3868860252fb9
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-04-09 15:09:33 +0300

    Update index.js

--- week2/index.js
+++ week2/index.js
@@ -3,13 +3,28 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const db = require('./database/db');
 const stationRoute = require('./routes/stationRoute');
+const connectionRoute = require('./routes/connectionRoute');
+const connectionTypeRoute = require('./routes/connectionTypeRoute');
+const currentTypeRoute = require('./routes/currentTypeRoute');
+const levelRoute = require('./routes/levelRoute');
+const userRoute = require('./routes/userRoute');
+const authRoute = require('./routes/authRoute');
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
+app.use('/user', userRoute);
+app.use('/auth', authRoute);
 app.use('/station', stationRoute);
+app.use('/connection', connectionRoute);
+app.use('/connectiontype', connectionTypeRoute);
+app.use('/currenttype', currentTypeRoute);
+app.use('/level', levelRoute);
 
 db.on('connected', () => {
   app.listen(3000);
